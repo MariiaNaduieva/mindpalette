8024e4e64ae78c91e563f99fbc39eb74530ce17a
Mariia Naduieva <masha.nadyeva@gmail.com>
2026-01-03 01:04:46 +0100

    Add starter page for creating and joining rooms

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to MindPalette</h1>
+      <button>Create Room</button>
+      <button>Join Room</button>
+    </main>
+  );
+}
